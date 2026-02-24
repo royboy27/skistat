054b25fde9223ba3c74fa2da2be72ab0066270b3
royboy27 <iamnoon962@gmail.com>
2026-02-23 21:10:28 -0800

    Update index.js

--- skistat-backend/src/index.js
+++ skistat-backend/src/index.js
@@ -7,6 +7,7 @@ const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 // ==========================================
