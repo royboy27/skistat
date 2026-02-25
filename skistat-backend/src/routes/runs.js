0cb599bfabcc7bd89e882bb1d0142947caa487f7
royboy27 <iamnoon962@gmail.com>
2026-02-24 20:05:19 -0800

    Update runs.js

--- skistat-backend/src/routes/runs.js
+++ skistat-backend/src/routes/runs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { authenticate } = require('../middleware/auth');
-const { query } = require('../config/database');
+const { query } = require('../../config/database');
 
 const router = express.Router();
 
